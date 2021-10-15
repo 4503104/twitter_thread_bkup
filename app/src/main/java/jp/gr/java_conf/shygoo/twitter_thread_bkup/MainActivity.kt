5cd94943489145789dffa8eb479eaac526b2091d
4503104 <shygoo@nifty.com>
2021-10-15 14:09:50 +0900

    Initial commit

---
+++ app/src/main/java/jp/gr/java_conf/shygoo/twitter_thread_bkup/MainActivity.kt
@@ -0,0 +1,11 @@
+package jp.gr.java_conf.shygoo.twitter_thread_bkup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
