5abae3af17d4eb995f04d7e498ee50b8518e167c
4503104 <shygoo@nifty.com>
2021-10-16 00:27:11 +0900

    Enable to input SpreadSheet URL

--- app/src/main/java/jp/gr/java_conf/shygoo/twitter_thread_bkup/MainActivity.kt
+++ app/src/main/java/jp/gr/java_conf/shygoo/twitter_thread_bkup/MainActivity.kt
@@ -1,11 +1,16 @@
 package jp.gr.java_conf.shygoo.twitter_thread_bkup
 
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.Group
 import androidx.core.view.isVisible
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
@@ -20,6 +25,9 @@ class MainActivity : AppCompatActivity() {
     private val mainMessage: TextView by lazy { findViewById(R.id.main_message) }
     private val signInButton: SignInButton by lazy { findViewById(R.id.sign_in_button) }
     private val signOutButton: Button by lazy { findViewById(R.id.sign_out_button) }
+    private val urlFormGroup: Group by lazy { findViewById(R.id.url_form_group) }
+    private val sheetUrlForm: EditText by lazy { findViewById(R.id.sheet_url_form) }
+    private val backUpButton: Button by lazy { findViewById(R.id.backup_button) }
 
     private val startSignIn = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
@@ -38,6 +46,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        backUpButton.setOnClickListener { executeBackUp() }
         prepareGoogleSignIn()
     }
 
@@ -58,11 +67,13 @@ class MainActivity : AppCompatActivity() {
             // Not signed yet
             signInButton.isVisible = true
             signOutButton.isVisible = false
+            urlFormGroup.isVisible = false
             mainMessage.text = getString(R.string.sign_in_message)
         } else {
             // Already signed in
             signInButton.isVisible = false
             signOutButton.isVisible = true
+            urlFormGroup.isVisible = true
             mainMessage.text = getString(R.string.already_signed_in_message, account.displayName)
         }
     }
@@ -76,9 +87,45 @@ class MainActivity : AppCompatActivity() {
         updateUi(null)
     }
 
+    private fun executeBackUp() {
+        val maybeSheetId = parseSheetIdFromInputUrl()
+        if (maybeSheetId == null) {
+            showToast(R.string.error_message_invalid_url)
+            return
+        }
+        showToast("id: $maybeSheetId")
+    }
+
+    private fun parseSheetIdFromInputUrl(): String? {
+        val maybeUrl = sheetUrlForm.text?.toString()
+        if (maybeUrl.isNullOrEmpty() || maybeUrl.startsWith(SHEET_URL_PREFIX_PETTERN).not()) {
+            return null
+        }
+        val uri = Uri.parse(maybeUrl)
+        val pathSegments = uri.pathSegments
+        if (pathSegments.size < SHEET_URL_MINIMUM_PATH_COUNT) {
+            return null
+        }
+        return pathSegments[SHEET_URL_SHEET_ID_POSITION]
+    }
+
+    private fun showToast(@StringRes messageId: Int) = showToast(getString(messageId))
+
+    private fun showToast(message: String) =
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
     companion object {
         private const val TAG = "MainActivity"
         /** Read & Write endpoint. https://developers.google.com/sheets/api/guides/authorizing */
         private const val SHEETS_API_ENDPOINT = "https://www.googleapis.com/auth/spreadsheets"
+
+        private const val SHEET_URL_SCHEME = "https"
+        private const val SHEET_URL_HOST = "docs.google.com"
+        private const val SHEET_URL_PATH1 = "spreadsheets"
+        private const val SHEET_URL_PATH2 = "d"
+        private const val SHEET_URL_PREFIX_PETTERN =
+            "$SHEET_URL_SCHEME://$SHEET_URL_HOST/$SHEET_URL_PATH1/$SHEET_URL_PATH2/"
+        private const val SHEET_URL_MINIMUM_PATH_COUNT = 3
+        private const val SHEET_URL_SHEET_ID_POSITION = 2
     }
 }
